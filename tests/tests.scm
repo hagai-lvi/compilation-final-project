a2c22a83718db3a2009e99792975bb96f1a3b7f0
hagai-lvi <hagai.lvi@gmail.com>
2015-01-05 21:11:19 +0200

    removed folder

--- tests/tests.scm
+++ tests/tests.scm
@@ -1,7 +1,6 @@
 ;;; Copyright (c) 2012 Andrew W. Keep
 ;;; See the accompanying file Copyright for detatils
 (load "compiler.scm")
-(load "folder.scm")
 (import
 	(rnrs)
 	(rough-draft unit-test)
@@ -58,101 +57,6 @@
 	)
 )
 
-(define-test-suite folder-tests
-	(define-test test-flatten
-		(assert-equal? (flatten `+ `(+ 1 2)) `(+ 1 2))
-		(assert-equal? (flatten `* `(+ 1 2)) `(+ 1 2))
-		(assert-equal? (flatten `+ `(+ 1 2 (+ 1 2) )) `(+ 1 2 1 2))
-		(assert-equal? (flatten `+ `(+ 1 2 (+ 3 4 (+ 5 6)) )) `(+ 1 2 3 4 (+ 5 6))) ; flatten only *one* level
-	)
-
-	(define-test test-fold-plus
-		(assert-equal? (fold `(+ 1 2 (+ (+ 1 2) a b))) `(+ 6 a b) )
-		(assert-equal? (fold `(+ 1 2 (+ a (+ 1 2) b))) `(+ 6 a b) )
-		(assert-equal? (fold `(+ 1 2 (+ (* 10 2) a a b))) `(+ 23 a a b) )
-	)
-
-	(define-test test-fold-add1
-		(assert-equal? (fold `(add1 (+ 2 3))) 6)
-		(assert-equal? (fold `(add1 #t)) '(+ 1 #t))
-		(assert-equal? (fold `(add1 (+ 3 4 (* 2 3) ))) 14)
-	)
-
-	(define-test test-fold-sub1
-		(assert-equal? (fold `(sub1 (+ 2 3))) 4)
-		(assert-equal? (fold `(sub1 #t)) '(+ -1 #t))
-		(assert-equal? (fold `(sub1 (+ 3 4 (* 2 3) ))) 12)
-	)
-
-	(define-test test-fold-number?
-		(assert-equal?  (fold `(number? 1)) #t )
-		(assert-equal?  (fold `(number? '1)) #f)
-		(assert-equal?  (fold `(number? (car '(1 a)))) #t )
-		(assert-equal?  (fold `(number? (car (cdr '(1 a))))) #f )
-	)
-
-	(define-test test-fold-null?
-		(assert-equal? (fold `(null? '())) #t )
-		(assert-equal? (fold `(null? (car (list '())))) #t )
-		(assert-equal? (fold `(null? '(1))) #f )
-		(assert-equal? (fold `(null? (car (list '(1) '(2) '(3) )))) #f )
-		(assert-equal? (fold `(null? (append '() '() ) )) #t )
-	)
-
-	(define-test test-fold-zero?
-		(assert-equal? (fold `(zero? 0)) #t )
-		(assert-equal? (fold `(zero? (sub1 1))) #t )
-		(assert-equal? (fold `(zero? 'a)) `(zero? 'a) )
-		(assert-equal? (fold `(zero? "a")) `(zero? "a") )
-		(assert-equal? (fold `(zero? (car '(1 a)) )) #f )
-		(assert-equal? (fold `(zero? (car '(0 a)) )) #t )
-	)
-
-	(define-test test-fold-string?
-		(assert-equal? (fold `(string? "abc")) #t )
-		(assert-equal? (fold `(string? (car '("abc")))) #t )
-		(assert-equal? (fold `(string? 1)) #f )
-		(assert-equal? (fold `(string? 'a)) #f )
-		(assert-equal? (fold `(string? x)) '(string? x) )
-	)
-
-	(define-test test-fold-string-append
-		(assert-equal? (fold `(string-append "a" "b"))  "ab")
-		(assert-equal? (fold `(string-append "a" a))  '(string-append "a" a))
-		(assert-equal? (fold `(string-append))  "")
-		(assert-equal? (fold `(string-append "a" (string-append "b" "c")))  "abc")
-	)
-
-	(define-test test-fold-if
-		;;;;;; Tests for (if pred dit dif) ;;;;;;;
-		(assert-equal? (fold `(if a b c)) '(if a b c) )
-		(assert-equal? (fold `(if #t b c)) 'b )
-		(assert-equal? (fold `(if #f b c)) 'c )
-		(assert-equal? (fold `(if (add1 1) (+ 3 4) c)) 7 )
-		(assert-equal? (fold `(if (car '(#t #f)) 1 2)) 1)
-		(assert-equal? (fold `(if (car (cdr '(#t #f))) 1 2)) 2)
-		;;;;;; Tests for (if pred dit) ;;;;;;;
-		(assert-equal? (fold `(if a b)) '(if a b) )
-		(assert-equal? (fold `(if #t b )) 'b )
-		(assert-equal? (fold `(if #f b )) *void-object* )
-		(assert-equal? (fold `(if (add1 1) (+ 3 4) )) 7 )
-		(assert-equal? (fold `(if (car '(#t #f)) 1 )) 1)
-		(assert-equal? (fold `(if (car ( cdr'(#t #f))) 1 )) *void-object*)
-	)
-
-	(define-test test-fold-list
-		(assert-equal? (fold '(list)) ''() )
-		(assert-equal? (fold '(list 1 2 3)) ''(1 2 3) )
-		(assert-equal? (fold '(list 1 x 3)) '(list 1 x 3) )
-	)
-
-	(define-test test-cars-and-cdrs
-		(assert-equal? (fold '(car (cdr '(1 2 3)))) 2)
-		(assert-equal? (fold '(car (cdr (list 1 2 3)))) 2)
-		(assert-equal? (fold '(car (cdr (cons 'a (cons 'b 'c))))) ''b)
-	)
-)
-
 
 (define-test-suite foo
 
@@ -196,5 +100,4 @@
 (exit (+ (run-test-suites
 							parse-tests
 							compiler-tests
-							folder-tests
 						)))
