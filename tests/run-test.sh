869a08b6d319379ca067b104aa790b3a3d0120c3
hagai-lvi <hagai.lvi@gmail.com>
2015-01-05 19:30:05 +0200

    a

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -3,7 +3,6 @@ set -o verbose
 echo
 echo ----------------------------------------------------------------------------------------------------------------
 echo 
-ls tests
 petite -q tests/tests.scm --libdirs "tests/rough-draft-master/src/"
 
 echo
