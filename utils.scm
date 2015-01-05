4b73b6c32ffba561a83eedc52d4b9939953e19e5
hagai-lvi <hagai.lvi@gmail.com>
2015-01-05 19:56:09 +0200

    moved seperate-last-element to utils.scm

--- utils.scm
+++ utils.scm
@@ -5,3 +5,17 @@
 
 
 (define (id x) x)
+
+(define with (lambda (s f)
+					(apply f s)))
+
+
+; return a pair that contain the head of the list and the last element of the list
+; example: (seperate-last-element '(1 2 3 4) returns '((1 2 3) . 4)
+(define (seperate-last-element list)
+	(letrec ((f (lambda (list succ)
+					(if (null? (cdr list))
+					    (succ `() (car list))
+					    (f (cdr list) (lambda (rest last)
+					    					(succ (cons (car list) rest) last)))))))
+	(f list (lambda (x y) (cons x y)))))
