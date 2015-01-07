56f40935b45af4a452b2fecf1326cb268e881ca6
coolmeen <ilyamor@post.bgu.ac.il>
2015-01-07 22:51:48 +0200

    d

--- output_test.c
+++ output_test.c
@@ -3,37 +3,99 @@
 
 /* change to 1 for debug info to be printed: */
 #define DO_SHOW 1
-
 #include "arch/cisc.h"
-#include "macros.h"
+
 int main()
 {
   START_MACHINE;
 
-  JUMP(CONTINUE);
+  
+//empty env
+PUSH(IMM(1));
+CALL(MALLOC);
+MOV(R12,R0)
+DROP(1);
+//***
 
-#include "arch/char.lib"
-#include "arch/io.lib"
-#include "arch/math.lib"
-#include "arch/string.lib"
-#include "arch/system.lib"
-#include "arch/scheme.lib"
 
- CONTINUE:
+    JUMP(CONTINUE);
+
+  #include "functions.lib"
+
+   CONTINUE:
 MAKE_INTEGER(2);
 PUSH(R0); 
+MAKE_INTEGER(2);
+PUSH(R0); 
+MAKE_INTEGER(2);
+PUSH(R0); 
+MAKE_INTEGER(2);
+PUSH(R0); 
+MAKE_INTEGER(3);
+PUSH(R0); 
 //end of params
-PUSH(IMM(1));
-MOVE_PVAR(0);
-PUSH(R0);
+PUSH(IMM(2));
+//**************proc code**********
+MAKE_CLOSURE(CONS);
+//**************proc code**********
 CMP(IND(R0),T_CLOSURE);
-JUMP_NE(Lnot_proc);
-MOV(R1,INDD(R0 , IMM(1)));
+JUMP_NE(lnot_proc);
+MOV(R1,INDD(R0 , IMM(1))); //push env
 PUSH(R1);
-CALL(*(INDD(R0 , IMM(2))));
-DROP(IMM(2+SCMNARGS));
+CALL(*(INDD(R0 , IMM(2)))); // jump to code label
+MOV(R1,SCMNARGS);
+ADD(R1,2);
+DROP(IMM(R1)); //remove all
+PUSH(R0); 
+//end of params
+PUSH(IMM(2));
+//**************proc code**********
+MAKE_CLOSURE(CONS);
+//**************proc code**********
+CMP(IND(R0),T_CLOSURE);
+JUMP_NE(lnot_proc);
+MOV(R1,INDD(R0 , IMM(1))); //push env
+PUSH(R1);
+CALL(*(INDD(R0 , IMM(2)))); // jump to code label
+MOV(R1,SCMNARGS);
+ADD(R1,2);
+DROP(IMM(R1)); //remove all
+PUSH(R0); 
+//end of params
+PUSH(IMM(2));
+//**************proc code**********
+MAKE_CLOSURE(CONS);
+//**************proc code**********
+CMP(IND(R0),T_CLOSURE);
+JUMP_NE(lnot_proc);
+MOV(R1,INDD(R0 , IMM(1))); //push env
+PUSH(R1);
+CALL(*(INDD(R0 , IMM(2)))); // jump to code label
+MOV(R1,SCMNARGS);
+ADD(R1,2);
+DROP(IMM(R1)); //remove all
+PUSH(R0); 
+//end of params
+PUSH(IMM(2));
+//**************proc code**********
+MAKE_CLOSURE(CONS);
+//**************proc code**********
+CMP(IND(R0),T_CLOSURE);
+JUMP_NE(lnot_proc);
+MOV(R1,INDD(R0 , IMM(1))); //push env
+PUSH(R1);
+CALL(*(INDD(R0 , IMM(2)))); // jump to code label
+MOV(R1,SCMNARGS);
+ADD(R1,2);
+DROP(IMM(R1)); //remove all
 // This code is to be appended to the final cisc file created by our compiler
-Lnot_proc:
+PUSH(R0);
+CALL(WRITE_SOB);
+CALL(NEWLINE);
+
+	lnot_proc:
+  l_NOT_VALID_ARGUMENTS:	
+  l_NOT_PAIR:
   STOP_MACHINE;
 
   return 0;
