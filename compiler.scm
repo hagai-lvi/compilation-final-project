9673590ef8e4cc39bb95d7a9162a2e5de017e3e7
hagai-lvi <hagai.lvi@gmail.com>
2015-01-09 21:30:04 +0200

    a

--- compiler.scm
+++ compiler.scm
@@ -569,7 +569,7 @@
 	(lambda (e)
 		(let* (	(vars (cadr e))
 				(body (caddr e))
-				(numOfVars (number->string (length vars) ))//TODO this is wrong! get numOfVars from the stack
+				(numOfVars (number->string (length vars) )) ;TODO this is wrong! get numOfVars from the stack
 				(label-copy-old-env (^label-lambda-copy-old-env))
 				(label-make-new-env (^label-lambda-make-new-env))
 				(label-code (^label-lambda-code))
