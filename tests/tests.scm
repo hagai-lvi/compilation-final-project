1bf5e09f1ed17b632c93ebaca1b6873cb247cc3f
hagai-lvi <hagai.lvi@gmail.com>
2015-01-16 15:11:45 +0200

    Added tests for get-const-location

--- tests/tests.scm
+++ tests/tests.scm
@@ -61,6 +61,20 @@
 		(assert-equal? (make-const-table '(1 "a" 3)) `((1 1 (T_INTEGER 1)) (3 "a" (T_STRING 1 97)) (6 3 (T_INTEGER 3))))
 		(assert-equal? (make-const-table '(1 "abc" 3)) `((1 1 (T_INTEGER 1)) (3 "abc" (T_STRING 3 97 98 99)) (6 3 (T_INTEGER 3))))
 	)
+
+	(define-test test-get-const-location
+		(assert-equal?	(get-const-location 1 (make-const-table '(1 2 3)))
+						1)
+
+		(assert-equal?	(get-const-location 2 (make-const-table '(1 2 3)))
+						3)
+
+		(assert-equal?	(get-const-location "2" (make-const-table '(1 "2" 3)))
+						3)
+
+		(assert-equal?	(get-const-location "abc" (make-const-table '(1 2 "a" "abc" 3)))
+						8)
+	)
 )
 
 
