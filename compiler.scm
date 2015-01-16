3242ecf91fac6fc572207036c7396dae01e1440d
hagai-lvi <hagai.lvi@gmail.com>
2015-01-16 15:49:47 +0200

    fixed const strings handling

--- compiler.scm
+++ compiler.scm
@@ -755,11 +755,14 @@
 		 		 		((and (boolean? e) (not e))
 		 		 			(f rest `(,@current-list (,counter ,e (T_BOOL 0))) (+ counter 2)))
 		 		 		((string? e)
-		 		 			(f rest `(,@current-list (,counter ,e (T_STRING ,(string-length e) ,@(get-ascii-list e)))) (+ counter 3 )))
-		 		 		(else 'fail)))) ; TODO exception? error?
-		)))
+		 		 			(f rest `(,@current-list (,counter ,e (T_STRING ,(string-length e) ,@(get-ascii-list e)))) (+ counter 2 (string-length e))))
+		 		 		;((pair? e)
+		 		 		;	(let (	(the-car (get-cons-location (car e)))
+		 		 		;			(the-cdr (get-cons-location (cdr e))))
+		 		 		;		(f rest `(,@current-list (,counter ,e (T_PAIR ,the-car ,the-cdr))) (+ counter 3 ))))
+		 		 		(else 'fail))))))) ; TODO exception? error?
 	(lambda (exp)
-		(f exp '() 1))))
+		(f exp (get-initial-const-list) 1))))
 
 
 (define topo-sort (trace-lambda topo-sort(exp) 
