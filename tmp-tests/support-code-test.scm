b3afc9a622e5bd642f11e2a98089dcfe8ef768dd
hagai-lvi <hagai.lvi@gmail.com>
2015-02-17 17:40:26 +0200

    fixed tests

--- tmp-tests/support-code-test.scm
+++ tmp-tests/support-code-test.scm
@@ -828,30 +828,6 @@
    (if (zero? n) #t
        (x x (- n 1)))))
 
-(define test
-  (lambda ()
-    (let* ((f (lambda (s)
-    (string->symbol
-     (symbol->string s))))
-     (g1 (gensym))
-     (g2 (gensym))
-     (g3 (gensym)))
-      (and (eq? g1 g1)
-     (eq? g2 g2)
-     (eq? g3 g3)
-     (not (eq? g1 g2))
-     (not (eq? g2 g3))
-     (not (eq? g1 g3))
-     (eq? (f g1)
-    (f (f g1)))
-     (eq? (f (f g2))
-    (f (f (f (f (f g2))))))
-     (not (eq? g1 (f g1)))
-     (not (eq? g3 (f (f (f (f (f g3)))))))
-     (eq? (f (f g2))
-    (f (f (f (f (f (f (f (f g2)))))))))))))
-(test)
-
 (((((lambda (x) (x (x x)))
     (lambda (x)
       (lambda (y)
