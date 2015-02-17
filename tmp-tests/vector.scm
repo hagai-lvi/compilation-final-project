320f3a5ffcbcebe39b6596378444946055641f8a
hagai-lvi <hagai.lvi@gmail.com>
2015-02-17 18:24:18 +0200

    fixed vector test

--- tmp-tests/vector.scm
+++ tmp-tests/vector.scm
@@ -6,12 +6,15 @@
 (vector-length (make-vector 2))
 (vector-length (make-vector 3 '((2 3) 2 3 (2 3))))
 (vector-length (make-vector 0 '((2 3) 2 3 (2 3))))
-(vector-set! (vector-set! (vector-set! (make-vector 3 #\a) 0 "this") 1 "is") 2 "sparta")
-(vector-set! (make-vector 1 #\a) 0 "abc")
+(define a (make-vector 3 #\a))
+(vector-set! a 0 "this")
+(vector-set! a 1 "is")
+(vector-set! a 2 "sparta")
+a
+(define b (make-vector 1 #\a))
+(vector-set! b 0 "abc")
+b
 
-
-;actually should be void but for testing
-
-(vector-ref (vector-set! (vector-set! (vector-set! (make-vector 3 #\a) 0 "this") 1 "is") 2 "sparta") 0)
-(vector-ref (vector-set! (vector-set! (vector-set! (make-vector 3 #\a) 0 "this") 1 "is") 2 "sparta") 1)
-(vector-ref (vector-set! (vector-set! (vector-set! (make-vector 3 #\a) 0 "this") 1 "is") 2 "sparta") 2)
+(vector-ref a 0)
+(vector-ref a 1)
+(vector-ref a 2)
